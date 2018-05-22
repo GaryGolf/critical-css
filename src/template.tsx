c83fa7e481f9f629520a3a8f3ad23874703fec1b
GaryGolf <garygolf@mail.ru>
2018-05-22 18:03:21 +0300

    minor changes

--- src/template.tsx
+++ src/template.tsx
@@ -5,16 +5,16 @@ interface Props {
 }
 export const HTML: React.SFC<Props> = props => {
     return (
-        <html>
-            <head>
-                <meta charSet="UTF-8" />
-                <meta name="viewport" content="width=device-width,initial-scale=1" />
-            </head>
-            <body>
-                <div id="app">{props.children}</div>
-                {/* <script src="/static/bundle.js" async /> */}
-                {/* <link rel="stylesheet" href="/static/styles.css" /> */}
-            </body>
-        </html>
+			<html>
+				<head>
+					<meta charSet="UTF-8" />
+					<meta name="viewport" content="width=device-width,initial-scale=1" />
+				</head>
+				<body>
+					<div id="app">{props.children}</div>
+					<script src="/static/bundle.js" async />
+					<link rel="stylesheet" href="/static/styles.css" />
+				</body>
+			</html>
     );
 }
