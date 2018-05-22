0cbf613289919626e3d97bc9f5fb6e24ef22ab8b
GaryGolf <garygolf@mail.ru>
2018-05-22 07:39:30 +0300

    create home component

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+interface Props {}
+
+export default class HomePage extends React.PureComponent<Props, null> {
+  render() {
+    return (
+      <div>
+        <h1> Home Page </h1>
+      </div>
+    );
+  }
+}
