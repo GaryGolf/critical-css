73780ad345e5e671e3a445ac659686064abf5217
GaryGolf <garygolf@mail.ru>
2018-05-22 09:01:27 +0300

    create client container

---
+++ src/client.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import HomePage from './components/home';
+
+ReactDOM.render(
+  <HomePage/>,
+  document.getElementById('app')
+)
