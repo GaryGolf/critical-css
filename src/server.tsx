46f0505b788eb3625db9deebcc671024180f8b10
GaryGolf <garygolf@mail.ru>
2018-08-22 18:06:37 +0300

    add cors to server

--- src/server.tsx
+++ src/server.tsx
@@ -1,15 +1,16 @@
+import cors from 'cors';
 import * as React from 'react';
 import * as ReactServer from 'react-dom/server';
 import { Express, Request, Response } from 'express';
 import { StaticRouter as Router } from 'react-router-dom';
 import { HTML } from './template';
 import routes from './routes';
-import HomePage from './components/home';
 const express = require('express');
 const port = 3000;
 const app: Express = express();
 
 app.use('/static', express.static(process.cwd() + '/dist'));
+app.use(cors());
 app.use((req:Request, res:Response) => {
   const context: { status?: number } = {};
   if (context.status && context.status >= 200 && context.status < 600) {
