493a07be6779be92c005b1414f37c44b60d32ca3
GaryGolf <garygolf@mail.ru>
2018-08-25 16:21:33 +0300

    add load date method to routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Dispatch } from 'redux';
-import { Switch, Route, matchPath, match } from 'react-router-dom';
+import { Switch, Route, matchPath } from 'react-router-dom';
 // containers;
 import HomePage from './components/home';
 import AboutPage from './components/about';
@@ -15,10 +15,20 @@ const routes = (
 );
 
 export const getRoutes = ():JSX.Element => routes;
-export const matchRoutes = (path:string):match<any>[] => routes
+export const matchRoutes = (path:string) => routes
   .props
   .children
   .reduce((acc, route) => {
     const m = matchPath(path, route.props);
-    return !m ? acc : [...acc, m];
-  } ,[])
+    return !m ? acc : [...acc, { ...m, component: route.props.component}];
+  } ,[]);
+
+export const loadData = (path:string) => routes
+  .props
+  .children
+  .reduce((acc, route) => {
+    const m = matchPath(path, route.props);
+    if (!m || !route.component.loadData) return acc;
+    return [...acc, route.component.loadData]
+  } ,[]);
+
