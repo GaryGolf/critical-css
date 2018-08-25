ed59e928238892e70feb5e24f81a5476a53e1220
GaryGolf <garygolf@mail.ru>
2018-08-25 16:20:34 +0300

    install serialize dependency

--- src/template.tsx
+++ src/template.tsx
@@ -1,20 +1,26 @@
 import * as React from "react";
+import * as serialize from 'serialize-javascript'
 
 interface Props {
-    children?: JSX.Element;
+	store?: any;
+  children?: JSX.Element;
 }
 export const HTML: React.SFC<Props> = props => {
-    return (
-			<html>
-				<head>
-					<meta charSet="UTF-8" />
-					<meta name="viewport" content="width=device-width,initial-scale=1" />
-				</head>
-				<body>
-					<div id="app">{props.children}</div>
-					<script src="/static/bundle.js" async />
-					<link rel="stylesheet" href="/static/styles.css" />
-				</body>
-			</html>
-    );
+	const store = serialize(props.store);
+  return (
+    <html>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="stylesheet" href="/static/styles.css" />
+      </head>
+      <body>
+        <div id="app">{props.children}</div>
+        { props.store && 
+          <script dangerouslySetInnerHTML={{ __html: `window.__PRELOADED_STATE__ = ${store};`}}/>
+        }
+        <script src="/static/bundle.js" async />
+      </body>
+    </html>
+  );
 }
