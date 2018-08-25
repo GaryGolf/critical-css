46d3254f0a2a2d8945491a1c048c3a60dc314655
GaryGolf <garygolf@mail.ru>
2018-08-25 16:22:11 +0300

    add load date method to message component

--- src/components/message/index.tsx
+++ src/components/message/index.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
-import AppStore from 'store/interfaces';
+import { Dispatch } from 'redux';
+import AppStore from '../../store/interfaces';
+import { setMessage } from '../../store/actions'
+
 const { connect } = require('react-redux');
 
 interface Props {
   msg?: string;
+  dispatch?: Dispatch;
 }
 
 @connect(
@@ -12,6 +16,11 @@ interface Props {
   })
 )
 export default class Messages extends React.PureComponent<Props, null> {
+
+  static loadData = (dispatch:Dispatch) => {
+    return Promise.all([dispatch(setMessage('hello world'))]);
+  }
+
   render() {
     return <h4>{this.props.msg}</h4>
   }
